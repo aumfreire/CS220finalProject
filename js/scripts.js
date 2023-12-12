b325c3ebd17ddb27f03652ba21cac944dfefdaa9
aumfreire <amf212@ensign.edu>
2023-12-11 19:12:21 -0700

    donation page / URL

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,33 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+document.addEventListener('DOMContentLoaded', function () {
+    var customAmountInput = document.getElementById('custom_amount');
+    var donationButtons = document.querySelectorAll('.donation-option');
+
+    donationButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            donationButtons.forEach(function (btn) {
+                btn.classList.remove('active');
+            });
+            button.classList.add('active');
+        });
+    });
+
+    customAmountInput.addEventListener('focus', function () {
+        donationButtons.forEach(function (button) {
+            button.classList.remove('active');
+        });
+    });
+
+    customAmountInput.addEventListener('blur', function () {
+        if (customAmountInput.value.trim() !== '') {
+            customAmountInput.value = '$' + customAmountInput.value;
+        }
+    });
+});
+
+function popup(){
+    alert("Thank you for donating today!");
+    window.location.reload();
+}
